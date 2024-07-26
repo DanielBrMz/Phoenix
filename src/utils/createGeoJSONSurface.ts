01a5e6363f6a6d158075c651802d46430d4105a9
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-25 20:20:55 -0700

    Initial commit

--- src/utils/createGeoJSONSurface.ts
+++ src/utils/createGeoJSONSurface.ts
@@ -1,62 +1,70 @@
-import type { Feature, GeoJsonProperties, Geometry, Point } from 'geojson';
+import type { Feature, GeoJsonProperties, Geometry, Point } from "geojson";
 
 interface Coords {
   latitude: number;
   longitude: number;
 }
 
-const createGeoJSONCircles = (center: [number, number], radiusInKm: number, variation = 0.2): mapboxgl.GeoJSONSourceRaw => {
+const createGeoJSONCircles = (
+  center: [number, number],
+  radiusInKm: number,
+  variation = 0.2,
+): mapboxgl.GeoJSONSourceRaw => {
   const points = 64;
-  const circles = 10; // Número de círculos concéntricos
+  const circles = 10; // Número de círculos concéntricosss
 
   const coords: Coords = {
-      latitude: center[1],
-      longitude: center[0]
+    latitude: center[1],
+    longitude: center[0],
   };
 
   const features: Feature<Geometry, GeoJsonProperties>[] = [];
 
-  for(let j=0; j<circles; j++) {
+  for (let j = 0; j < circles; j++) {
     const km = radiusInKm * ((j + 1) / circles); // Radio del círculo concéntrico
 
     const ret: Point[] = [];
-    const distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
-    const distanceY = km/110.574;
+    const distanceX =
+      km / (111.32 * Math.cos((coords.latitude * Math.PI) / 180));
+    const distanceY = km / 110.574;
 
     let theta, x, y;
-    for(let i=0; i<points; i++) {
-        // Introduce some variation in the radius
-        const radiusVariation = 1 + Math.random() * variation - variation / 2;
-        const variedDistanceX = distanceX * radiusVariation;
-        const variedDistanceY = distanceY * radiusVariation;
+    for (let i = 0; i < points; i++) {
+      // Introduce some variation in the radius
+      const radiusVariation = 1 + Math.random() * variation - variation / 2;
+      const variedDistanceX = distanceX * radiusVariation;
+      const variedDistanceY = distanceY * radiusVariation;
 
-        // Introduce some variation in theta
-        const thetaVariation = (Math.random() - 0.5) * (2 * Math.PI / points);
-        theta = ((i / points) * (2 * Math.PI)) + thetaVariation;
+      // Introduce some variation in theta
+      const thetaVariation = (Math.random() - 0.5) * ((2 * Math.PI) / points);
+      theta = (i / points) * (2 * Math.PI) + thetaVariation;
 
-        x = variedDistanceX*Math.cos(theta);
-        y = variedDistanceY*Math.sin(theta);
+      x = variedDistanceX * Math.cos(theta);
+      y = variedDistanceY * Math.sin(theta);
 
-        ret.push({ type: 'Point', coordinates: [coords.longitude+x, coords.latitude+y] });
+      ret.push({
+        type: "Point",
+        coordinates: [coords.longitude + x, coords.latitude + y],
+      });
     }
-    if (ret[0])  ret.push(ret[0]);
+    if (ret[0]) ret.push(ret[0]);
 
     features.push({
-      "type": "Feature",
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [ret.map(point => point.coordinates)]
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        coordinates: [ret.map((point) => point.coordinates)],
       },
-      "properties": {}
-    });    
+      properties: {},
+    });
   }
 
   return {
-    "type": "geojson",
-    "data": {
-      "type": "FeatureCollection",
-      "features": features
-    }
+    type: "geojson",
+    data: {
+      type: "FeatureCollection",
+      features: features,
+    },
   };
 };
 
