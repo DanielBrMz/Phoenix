b5f9ccd76bb878e7f9fd7dbfbc8a89f93c9f5a82
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-29 02:55:57 -0700

    feat: Add emergency services and infrastructure data
    This commit adds the emergencyServices.ts and infrastructure.ts files, which contain data for emergency services and infrastructure icons respectively. The emergencyServices.ts file includes information about hospitals, firefighters, and police stations, while the infrastructure.ts file includes data about electricity poles. These files are necessary for displaying icons on the map.

---
+++ src/data/emergencyServices.ts
@@ -0,0 +1,43 @@
+import firefighter from "../assets/emergencyIcons/bombero.png";
+import hospital from "../assets/emergencyIcons/hospital.png";
+import police from "../assets/emergencyIcons/policia.png";
+
+export const emergencyServices = [
+  {
+    id: "e1",
+    icon: hospital,
+    name: "hospital",
+    coordinates: [31.27, -110.9],
+  },
+  {
+    id: "e2",
+    icon: hospital,
+    name: "hospital",
+    coordinates: [31.2655, -110.905],
+  },
+
+  {
+    id: "e3",
+    icon: firefighter,
+    name: "firefighter",
+    coordinates: [31.275, -110.9025],
+  },
+  {
+    id: "e4",
+    icon: firefighter,
+    name: "firefighter",
+    coordinates: [31.268, -110.898],
+  },
+  {
+    id: "e5",
+    icon: police,
+    name: "police",
+    coordinates: [31.2725, -110.8965],
+  },
+  {
+    id: "e6",
+    icon: police,
+    name: "police",
+    coordinates: [31.266, -110.899],
+  },
+];
---
+++ src/data/infrastructure.ts
@@ -0,0 +1,41 @@
+import electricityPole from "../assets/infrastructureIcons/torre.png";
+
+export const infrastructure = [
+  {
+    id: "i1",
+    icon: electricityPole,
+    name: "electricity_pole",
+    coordinates: [31.274, -110.903],
+  },
+  {
+    id: "i2",
+    icon: electricityPole,
+    name: "electricity_pole",
+    coordinates: [31.269, -110.8975],
+  },
+
+  {
+    id: "i3",
+    icon: electricityPole,
+    name: "electricity_pole",
+    coordinates: [31.271, -110.901],
+  },
+  {
+    id: "i4",
+    icon: electricityPole,
+    name: "electricity_pole",
+    coordinates: [31.2675, -110.9045],
+  },
+  {
+    id: "i5",
+    icon: electricityPole,
+    name: "electricity_pole",
+    coordinates: [31.273, -110.9005],
+  },
+  {
+    id: "i6",
+    icon: electricityPole,
+    name: "electricity_pole",
+    coordinates: [31.2685, -110.8995],
+  },
+];
