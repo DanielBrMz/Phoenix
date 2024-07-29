8f14f6de6bb993fa7e0f495f96a10711b1b42649
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-29 03:52:50 -0700

    Refactor emergencyServices coordinates for accuracy

--- src/data/emergencyServices.ts
+++ src/data/emergencyServices.ts
@@ -7,7 +7,7 @@ export const emergencyServices = [
     id: "e1",
     icon: hospital,
     name: "hospital",
-    coordinates: [-110.9, 31.27],
+    coordinates: [-110.894, 31.283],
   },
   {
     id: "e2",
@@ -19,24 +19,24 @@ export const emergencyServices = [
     id: "e3",
     icon: firefighter,
     name: "firefighter",
-    coordinates: [-110.9025, 31.275],
+    coordinates: [-110.882, 31.275],
   },
   {
     id: "e4",
     icon: firefighter,
     name: "firefighter",
-    coordinates: [-110.898, 31.268],
+    coordinates: [-110.905, 31.278],
   },
   {
     id: "e5",
     icon: police,
     name: "police",
-    coordinates: [-110.8965, 31.2725],
+    coordinates: [-110.911, 31.275],
   },
   {
     id: "e6",
     icon: police,
     name: "police",
-    coordinates: [-110.899, 31.266],
+    coordinates: [-110.888, 31.28],
   },
 ];
