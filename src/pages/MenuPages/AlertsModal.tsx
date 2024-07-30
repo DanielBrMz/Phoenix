ed416f5df4e7c5f4af42ef24f654cf799529fbe1
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-30 08:13:45 -0700

    AlertsSection finished

--- src/pages/MenuPages/AlertsModal.tsx
+++ src/pages/MenuPages/AlertsModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "~/styles/NavbarStyles/AlertModal.module.css";
+import styles from "~/styles/NavbarStyles/AlertSection.module.css";
 import alertsStore from "~/store/alertsStore";
 
 interface Alert {
@@ -25,27 +25,37 @@ const AlertsModal: React.FC<AlertsModalProps> = ({ alert }) => {
   };
 
   return (
-    <div className={styles.modal}>
-      <div className={styles.checkboxContainer}>
-        <input
-          type="checkbox"
-          checked={alertsVisible}
-          onChange={handleCheckbox}
-        />
-        <p>Emergency Alerts</p>
+    <div className={styles.alertsContainer}>
+      <h2 className={styles.alertsTitle}>ALERTS</h2>
+      <div className={styles.line}></div>
+      <div className={styles.checkboxContainer} onClick={handleCheckbox}>
+        <div
+          className={`${styles.checkboxAlerts} ${
+            alertsVisible ? styles.checkboxChecked : ""
+          }`}
+        ></div>
+        <h2 className={styles.checkboxAlertsSubtitle}>
+          EMERGENCY RESPONSE DEVICE
+        </h2>
       </div>
+      <div className={styles.line}></div>
       {alert ? (
-        <div className={styles.alertDetails}>
-          <h2>
+        <div className={styles.alertDetailsContainer}>
+          <h2 className={styles.alertDetailsTitle}>
             ALERT! A fire is predicted to reach the location in{" "}
-            {alert.hourPrediction} hours
+            {alert.hourPrediction}{" "}
+            {alert.hourPrediction == 1 ? "hour" : "hours"}
           </h2>
-          <p>Send at: {new Date(alert.sendTime).toLocaleString()}</p>
-          <p>Received at: {new Date(alert.receivedTime).toLocaleString()}</p>
+          <div className={styles.alertDetailsDescription}>
+            <p>Send at: {new Date(alert.sendTime).toLocaleString()}</p>
+            <p>Received at: {new Date(alert.receivedTime).toLocaleString()}</p>
+          </div>
         </div>
       ) : (
-        <p>No alert selected</p>
+        <p className={styles.noAlertText}>No alert selected</p>
       )}
+      <div className={styles.line}></div>
+      <button className={styles.alertExitButton}>EXIT</button>
     </div>
   );
 };
