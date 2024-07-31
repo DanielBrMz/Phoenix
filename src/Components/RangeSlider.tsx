52496d013e0916c32a4da36fbbe3723dea338c5e
HectorGtz27 <alan.gtz51@gmail.com>
2024-07-30 20:35:59 -0700

    refactor: Update RangeSlider component with lazy loading for thumbIcon

--- src/Components/RangeSlider.tsx
+++ src/Components/RangeSlider.tsx
@@ -4,10 +4,56 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import { setHeatmapRadius } from "~/utils/mapUtils/addCustomLayers";
 import styles from "~/styles/SliderStyles/RangeSlider.module.css";
+import thumbIcon from "~/assets/thumbIcon.png";
 
 interface RangeSliderProps {
   map: mapboxgl.Map;
 }
+
+const AirbnbSlider = styled(Slider)(({ theme }) => ({
+  color: "#f57f61",
+  height: 4,
+  "& .MuiSlider-thumb": {
+    height: 24,
+    width: 24,
+    backgroundColor: "#f57f61",
+    borderRadius: "3px",
+    borderColor: "white",
+    borderStyle: "solid",
+    borderWidth: 2,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    "&:hover": {
+      boxShadow: "0 0 0 8px rgba(58, 133, 137, 0.16)",
+    },
+    "&::before": {
+      content: '""',
+      display: "block",
+      width: "16px",
+      height: "16px",
+      backgroundImage: `url(${thumbIcon.src})`,
+      backgroundSize: "cover",
+    },
+  },
+  "& .MuiSlider-track": {
+    height: 4,
+  },
+  "& .MuiSlider-rail": {
+    height: 4,
+    backgroundColor: "white",
+    color: "white",
+  },
+  "& .MuiSlider-mark": {
+    backgroundColor: "white",
+    height: 12,
+    width: 12,
+    borderRadius: "50%",
+    "&.MuiSlider-markActive": {
+      backgroundColor: "#f57f61",
+    },
+  },
+}));
 
 export default function RangeSlider({ map }: RangeSliderProps) {
   const [value, setValue] = React.useState(0);
@@ -15,35 +61,6 @@ export default function RangeSlider({ map }: RangeSliderProps) {
   const min = 0;
   const max = 48;
   const thumbWidth = 24; // Ancho del thumb
-
-  const AirbnbSlider = styled(Slider)(({ theme }) => ({
-    color: "#57c5f7",
-    height: 4,
-    "& .MuiSlider-thumb": {
-      height: thumbWidth,
-      width: thumbWidth,
-      backgroundColor: "#fff",
-      border: "2px solid #000",
-      "&:hover": {
-        boxShadow: "0 0 0 8px rgba(58, 133, 137, 0.16)",
-      },
-    },
-    "& .MuiSlider-track": {
-      height: 4,
-    },
-    "& .MuiSlider-rail": {
-      height: 4,
-      backgroundColor: "#d8d8d8",
-    },
-    "& .MuiSlider-mark": {
-      backgroundColor: "#000",
-      height: 10,
-      width: 2,
-      "&.MuiSlider-markActive": {
-        backgroundColor: "transparent",
-      },
-    },
-  }));
 
   const calculateLeftPosition = () => {
     const position = ((value - min) / (max - min)) * sliderWidth;
