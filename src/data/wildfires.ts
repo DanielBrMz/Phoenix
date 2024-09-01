52c80be8cad45d5408c42e264809f72db807e474
HectorGtz27 <alan.gtz51@gmail.com>
2024-09-01 02:48:16 -0700

    feat: Add coordinates to wildfire details in wildfires.ts

--- src/data/wildfires.ts
+++ src/data/wildfires.ts
@@ -13,6 +13,7 @@ interface Wildfire {
   name: string;
   actualData: ActualData;
   weatherConditions: WeatherConditions;
+  coordinates: [number, number];
 }
 
 interface ActualData {
@@ -50,6 +51,7 @@ export const wildfiresDetails: Wildfires[] = [
           {
             id: "us_ca_1",
             name: "California Wildfire",
+            coordinates: [-117.16265298633692, 36.76902983954334],
             actualData: {
               startTime: "06/10/23 09:00",
               sheltering: "Fully Sheltered",
@@ -155,6 +157,7 @@ export const wildfiresDetails: Wildfires[] = [
           {
             id: "mex_no_1",
             name: "Nogales Wildfire",
+            coordinates: [-110.8968082457804, 31.26933620026809],
             actualData: {
               startTime: "07/29/24 9:00",
               sheltering: "Fully Sheltered",
@@ -179,87 +182,7 @@ export const wildfiresDetails: Wildfires[] = [
               brightness: "313.8",
             },
           },
-          {
-            id: "mex_im_1",
-            name: "Imuris Wildfire",
-            actualData: {
-              startTime: "06/10/23 09:00",
-              sheltering: "Fully Sheltered",
-              liveMoist: "Fully Cured",
-              elevDifference: "Below 1000 ft",
-              aspect: "North",
-              size: "231.4 ac",
-              fuel: "9 (TL)",
-              slope: "62° (188%)",
-              frp: "21.4",
-            },
-            weatherConditions: {
-              windDirection: "200°",
-              windEyeLevel: "10 mi/h",
-              airTemp: "32°F",
-              relHumidity: "10%",
-              precipitation: "0",
-              shading: "0-20%",
-              clouds: "0",
-              solarRadiation: "343.6",
-              heatIndex: "15.6",
-              brightness: "313.8",
-            },
-          },
-          {
-            id: "mex_im_1",
-            name: "Hermosillo Wildfire",
-            actualData: {
-              startTime: "06/10/23 09:00",
-              sheltering: "Fully Sheltered",
-              liveMoist: "Fully Cured",
-              elevDifference: "Below 1000 ft",
-              aspect: "North",
-              size: "231.4 ac",
-              fuel: "9 (TL)",
-              slope: "62° (188%)",
-              frp: "21.4",
-            },
-            weatherConditions: {
-              windDirection: "200°",
-              windEyeLevel: "10 mi/h",
-              airTemp: "32°F",
-              relHumidity: "10%",
-              precipitation: "0",
-              shading: "0-20%",
-              clouds: "0",
-              solarRadiation: "343.6",
-              heatIndex: "15.6",
-              brightness: "313.8",
-            },
-          },
-          {
-            id: "mex_im_1",
-            name: "Yecora Wildfire",
-            actualData: {
-              startTime: "06/10/23 09:00",
-              sheltering: "Fully Sheltered",
-              liveMoist: "Fully Cured",
-              elevDifference: "Below 1000 ft",
-              aspect: "North",
-              size: "231.4 ac",
-              fuel: "9 (TL)",
-              slope: "62° (188%)",
-              frp: "21.4",
-            },
-            weatherConditions: {
-              windDirection: "200°",
-              windEyeLevel: "10 mi/h",
-              airTemp: "32°F",
-              relHumidity: "10%",
-              precipitation: "0",
-              shading: "0-20%",
-              clouds: "0",
-              solarRadiation: "343.6",
-              heatIndex: "15.6",
-              brightness: "313.8",
-            },
-          },
+
           // Other wildfires...
         ],
       },
