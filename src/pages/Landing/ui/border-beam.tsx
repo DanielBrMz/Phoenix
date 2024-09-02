e80fab26f0137a9d104d633c8206f542c3f0f00f
HectorGtz27 <alan.gtz51@gmail.com>
2024-09-02 02:46:56 -0700

    Changing the borderbeam to orange

--- src/pages/Landing/ui/border-beam.tsx
+++ src/pages/Landing/ui/border-beam.tsx
@@ -17,8 +17,8 @@ export const BorderBeam = ({
   duration = 15,
   anchor = 90,
   borderWidth = 2,
-  colorFrom = "#ffaa40",
-  colorTo = "#9c40ff",
+  colorFrom = "#FFA500", // Naranja claro
+  colorTo = "#FF4500", // Naranja más oscuro
   delay = 0,
 }: BorderBeamProps) => {
   return (
@@ -37,10 +37,10 @@ export const BorderBeam = ({
       className={cn(
         "absolute inset-[0] rounded-[inherit] [border:calc(var(--border-width)*1px)_solid_transparent]",
 
-        // mask styles
+        // Estilos de máscara
         "![mask-clip:padding-box,border-box] ![mask-composite:intersect] [mask:linear-gradient(transparent,transparent),linear-gradient(white,white)]",
 
-        // pseudo styles
+        // Estilos pseudo
         "after:animate-border-beam after:absolute after:aspect-square after:w-[calc(var(--size)*1px)] after:[animation-delay:var(--delay)] after:[background:linear-gradient(to_left,var(--color-from),var(--color-to),transparent)] after:[offset-anchor:calc(var(--anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(var(--size)*1px))]",
         className,
       )}
